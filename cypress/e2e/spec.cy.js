a1bbc0a9553c410eb6ceea1664552d6108afb76b
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-03 10:11:07 +0100

    fix(cypress): correction fail 500

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://laravel.traefik.me');
+    cy.visit('https://laravel.traefik.me', {failOnStatusCode: false});
     cy.screenshot('first-page');
   })
 })
